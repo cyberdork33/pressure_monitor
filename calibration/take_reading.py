ae490ebde7d597397fc44a379b897b4106c7d342
cyberdork33 <cyberdork33@gmail.com>
2021-07-04 07:46:22 -0400

    Finish script to take readings for calibration

--- calibration/take_reading.py
+++ calibration/take_reading.py
@@ -2,11 +2,11 @@
 # cyberdork33 <cyberdork33@gmail.com>
 
 import time
+import datetime
+import csv
 
 # Import the ADS1x15 module.
-#from ADS1x15 import ADS1015
 from ADS1x15 import ADS1115
-# import ADS1x15
 
 # Create an ADS1115 ADC (16-bit) instance.
 # adc = ADS1x15.ADS1115()
@@ -24,26 +24,36 @@ adc = ADS1115()
 #  -  16 = +/-0.256V
 # See table 3 in the ADS1015/ADS1115 datasheet for more info on gain.
 GAIN = 1
-CHANNEL = 0
-DATA_RATE = 128
-READINGS = 10
+CHANNEL = 0 # which channel to read from
+DATA_RATE = 128 # what data rate to use. 128 is default.
+READINGS = 10 # how many readings to take.
 
 ## MAIN CODE
-print('Reading ADS1x15 values.')
 
-for i in range(READINGS):
-	# array to hold read values
-	# Values read will be a 16-bit signed integer value
-	# values = [0]
+with open('readings.csv', 'w', newline='', encoding='utf-8') as file:
+	writer = csv.writer(file)
+	writer.writerow(["Read Count", "Timestamp", "Voltage [V]", "Read Value"])
 
-	# Read the specified ADC channel using the previously set gain value.
-	values = adc.read_adc(CHANNEL, gain=GAIN)
-	# Note you can also pass in an optional data_rate parameter that controls
-	# the ADC conversion time (in samples/second). See datasheet Table 9 config 
-	# register DR bit values.
-	#values = adc.read_adc(CHANNEL, gain=GAIN, data_rate=DATA_RATE)
+	while True:
+		# Prompt for input voltage
+		print('Ctrl+C to quit')
+		print('What Voltage Level? >')
+		voltage = input()
 
-	# Print the ADC values.
-	print(' {0:>6} '.format(values))
-	# Pause for half a second.
-	time.sleep(0.5)
+		for i in range(READINGS):
+
+			# Get the Current Timestamp
+			ct = datetime.datetime.now()
+			
+			# Read the specified ADC channel using the previously set gain value.
+			# Values read will be a 16-bit signed integer value
+			value = adc.read_adc(CHANNEL, gain=GAIN)
+			
+			# Print the ADC values.
+			print(' {0:>6} '.format(value))
+
+			# Write the read values.
+			writer.writerow([i, ct, voltage, '{0:>6} '.format(value)])
+
+			# Pause for half a second between each reading
+			time.sleep(0.5)
