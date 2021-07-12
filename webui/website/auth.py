57d861df012fb1b29d5b118d34b38a179e41a903
cyberdork33 <cyberdork33@duke-arch>
2021-07-12 19:53:27 -0400

    implement user login controls

--- webui/website/auth.py
+++ webui/website/auth.py
@@ -2,18 +2,35 @@ from flask import Blueprint, request, flash, render_template, redirect, url_for
 from .models import User
 from werkzeug.security import generate_password_hash, check_password_hash
 from . import db
+from flask_login import login_user, login_required, logout_user, current_user
 
 auth = Blueprint('auth', __name__)
 
 @auth.route('/login', methods=['GET', 'POST'])
 def login():
-	data = request.form
-	print(data)
+	if request.method == 'POST':
+		email = request.form.get('email')
+		password = request.form.get('password')
+
+		# Get all users in db that have this email address
+		user = User.query.filter_by(email=email).first()
+		if user:
+			if check_password_hash(user.password, password):
+				flash('Logged in successfully!', category='success')
+				login_user(user, remember=True)
+				return redirect(url_for('views.admin'))
+			else:
+				flash('Password was incorrect. Try Again.', category='error')
+		else:
+			flash('Email not found.', category='error')
+
 	return render_template("login.html")
 
 @auth.route('/logout')
+@login_required
 def logout():
-	return "<p>Logout</p>"
+	logout_user()
+	return redirect(url_for('views.home'))
 
 @auth.route('/register', methods=['GET', 'POST'])
 def register():
@@ -23,22 +40,23 @@ def register():
 		password1 = request.form.get('password1')
 		password2 = request.form.get('password2')
 
-		# Can do some validation on on these if desired
-		if len(password1) < 8:
+		# Get all users in db that have this email address
+		user = User.query.filter_by(email=email).first()
+		if user:
+			flash('Email already exists.', category='error')
+		elif len(password1) < 8:
 			flash('Passwords must be at least 8 characters!', category='error')
 		elif password1 != password2:
 			flash('Passwords did not match!', category='error')
 		else:
 			# add user to database
 			flash('Creating Account.', category='success')
-			new_user = User(
-										email=email,
-										name=name,
-										password=generate_password_hash(password1, method='sha256')
-										)
+			new_user = User(email=email,
+											name=name,
+											password=generate_password_hash(password1, method='sha256')
+											)
 			db.session.add(new_user)
 			db.session.commit()
-			return redirect(url_for('views.home'))
-
+			return redirect(url_for('views.login'))
 
 	return render_template('register.html')
--- webui/website/views.py
+++ webui/website/views.py
@@ -1,11 +1,13 @@
 from flask import Blueprint, render_template
+from flask_login import login_required, current_user
 
 views = Blueprint('views', __name__)
 
 @views.route('/')
 def home():
-  return render_template("home.html")
+	return render_template("home.html", user=current_user)
 
-@views.route('/admin')
+@views.route('/admin', methods=['GET','POST'])
+@login_required
 def admin():
-  return render_template("admin.html", methods=['GET', 'POST'])
+	return render_template("admin.html", user=current_user)
