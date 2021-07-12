3fcf5061a33c88a2d68399752c17892071fa7fe9
cyberdork33 <cyberdork33@gmail.com>
2021-07-11 22:40:25 -0400

    start on forms for logging in.

--- webui/website/auth.py
+++ webui/website/auth.py
@@ -1,17 +1,34 @@
-from flask import Blueprint
+from flask import Blueprint, request, flash
 from flask.templating import render_template
 
 auth = Blueprint('auth', __name__)
 
-@auth.route('/login')
+@auth.route('/login', methods=['GET', 'POST'])
 def login():
-  # return "<p>Login</p>"
-  return render_template("login.html", text="Testing",  user="Tim")
+  data = request.form
+  print(data)
+  return render_template("login.html")
 
 @auth.route('/logout')
 def logout():
   return "<p>Logout</p>"
 
-@auth.route('/register')
+@auth.route('/register', methods=['GET', 'POST'])
 def register():
+  if request.method == 'POST':
+    email = request.form.get('email')
+    name = request.form.get('name')
+    password1 = request.form.get('password1')
+    password2 = request.form.get('password2')
+
+    # Can do some validation on on these if desired
+    if len(password1) < 8:
+      flash('Passwords must be at least 8 characters!', category='error')
+    elif password1 != password2:
+      flash('Passwords did not match!', category='error')
+    else:
+      # add user to database
+      flash('Creating Account.', category='success')
+      pass
+
   return render_template('register.html')
