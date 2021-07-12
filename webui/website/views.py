33f7932c48c9096becdc307ff634258995df04bc
cyberdork33 <cyberdork33@duke-arch>
2021-07-12 11:27:35 -0400

    get started on an admin home page

--- webui/website/views.py
+++ webui/website/views.py
@@ -5,3 +5,7 @@ views = Blueprint('views', __name__)
 @views.route('/')
 def home():
   return render_template("home.html")
+
+@views.route('/admin')
+def admin():
+  return render_template("admin.html", methods=['GET', 'POST'])
