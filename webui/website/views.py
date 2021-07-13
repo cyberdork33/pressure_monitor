48bd8947b03ad61299c4c51a5a3d23a6ff564805
cyberdork33 <cyberdork33@gmail.com>
2021-07-12 20:59:01 -0400

    wired up admin buttons

--- webui/website/views.py
+++ webui/website/views.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request, flash
 from flask_login import login_required, current_user
 
 views = Blueprint('views', __name__)
@@ -14,4 +14,15 @@ def about():
 @views.route('/admin', methods=['GET','POST'])
 @login_required
 def admin():
+	if request.method == 'POST':
+		button_clicked = request.form['submit_button']
+		if button_clicked == 'forceReading':
+			# Call the take-reading method
+			flash('A reading was taken.', category='success')
+		elif button_clicked == 'calibrateReading':
+			# Call the calibrate-reading method
+			flash('A calibration reading was taken.', category='success')
+		else:
+			flash('Unsure what reading to take.', category='error')
+
 	return render_template("admin.html", user=current_user)
