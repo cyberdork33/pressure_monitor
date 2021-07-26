a55e7307f70e6e3f48b28a97ef76c3657570ea83
cyberdork33 <cyberdork33@duke-arch>
2021-07-26 08:21:21 -0400

    setup cron to fetch readings regularly

--- webhost_app/main.py
+++ webhost_app/main.py
@@ -1,6 +1,15 @@
-from website import create_app
+import atexit
+from website import create_app, create_scheduler
 
 app = create_app()
+scheduler = create_scheduler(app)
 
 if __name__ == '__main__':
-	app.run(host='0.0.0.0' ,debug=True)
+  scheduler.start()
+  # 0.0.0.0 host allows connections from everwhere
+  # debug mode loads Flask app twice for some reason, and this
+  # causes issues with flask_apscheduler so, disable relaoder.
+  app.run(host='0.0.0.0', debug=True, use_reloader=False)
+
+  # Shut down the scheduler when exiting the app
+  atexit.register(lambda: scheduler.shutdown())
--- webhost_app/website/__init__.py
+++ webhost_app/website/__init__.py
@@ -1,43 +1,55 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_apscheduler import APScheduler
 from os import path
 from flask_login import LoginManager
 
 db = SQLAlchemy()
 DB_NAME = "site_db.sqlite3"
+READING_DELTA = 15 #minutes
 
 def create_app():
 
-	# General App Setup
-	app = Flask(__name__)
-	app.config['SECRET_KEY'] = '78efdeeb9a6135ae65acf8a797ede88d775047cb36a7c80c9f17f3e3'
-	app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{DB_NAME}'
-	app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False #Suppress Deprecation Warning
-	db.init_app(app)
+  # General App Setup
+  app = Flask(__name__)
+  app.config['SECRET_KEY'] = '78efdeeb9a6135ae65acf8a797ede88d775047cb36a7c80c9f17f3e3'
+  app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{DB_NAME}'
+  app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False #Suppress Deprecation Warning
+  db.init_app(app)
 
-	# Setup the site Blueprints
-	from .views import views
-	from .auth import auth
+  # Setup the site Blueprints
+  from .views import views
+  from .auth import auth
 
-	app.register_blueprint(views, url_prefix='/')
-	app.register_blueprint(auth, url_prefix='/')
+  app.register_blueprint(views, url_prefix='/')
+  app.register_blueprint(auth, url_prefix='/')
 
-	# Setup the site databse
-	from .models import User, CalibrationReading, MonitorReading
-	create_database(app)
+  # Setup the site databse
+  from .models import User, CalibrationReading, MonitorReading
+  create_database(app)
 
-	# Setup the Login Manager
-	login_manager = LoginManager()
-	login_manager.login_view = 'auth.login'
-	login_manager.init_app(app)
+  # Setup the Login Manager
+  login_manager = LoginManager()
+  login_manager.login_view = 'auth.login'
+  login_manager.init_app(app)
 
-	@login_manager.user_loader
-	def load_user(id):
-		return User.query.get(int(id))
+  @login_manager.user_loader
+  def load_user(id):
+    return User.query.get(int(id))
 
-	return app
+  return app
+
+def create_scheduler(app):
+  from .data_fetch import cron_reading
+  scheduler = APScheduler()
+  scheduler.add_job(id='Scheduled Task',
+                    func=cron_reading,
+                    kwargs={'app':app},
+                    trigger="interval",
+                    minutes=READING_DELTA)
+  return scheduler
 
 def create_database(app):
-	if not path.exists('website/' + DB_NAME):
-		db.create_all(app=app)
-		print("Created Database!")
+  if not path.exists('website/' + DB_NAME):
+    db.create_all(app=app)
+    print("Created Database!")
